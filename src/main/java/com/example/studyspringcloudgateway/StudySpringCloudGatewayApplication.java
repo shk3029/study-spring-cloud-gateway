5acf59f561746e6ec6144d2c2fbfed2a3be156b8
shk3029 <shk30290@naver.com>
2022-01-13 22:04:24 +0900

    Initial commit

---
+++ src/main/java/com/example/studyspringcloudgateway/StudySpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspringcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyspringcloudgateway/StudySpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyspringcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
